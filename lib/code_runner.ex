8c97f1be479d489a1c2600165ed6b810294ce20d
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-05-18 20:57:05 +0900

    Initial commit
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

---
+++ lib/code_runner.ex
@@ -0,0 +1,18 @@
+defmodule CodeRunner do
+  @moduledoc """
+  Documentation for CodeRunner.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CodeRunner.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
