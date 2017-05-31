133fc0a53f082ef6e7410ccd3c91998479c8cc66
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-05-31 21:39:49 +0900

    Add test case for isolating runnning environment
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- test/code_runner_test.exs
+++ test/code_runner_test.exs
@@ -12,6 +12,13 @@ defmodule CodeRunnerTest do
     assert CodeRunner.run(code) == "** (UndefinedFunctionError) function IO.puts/0 is undefined or private. Did you mean one of:\n\n      * puts/1\n      * puts/2\n\n    (elixir) IO.puts()\n    (stdlib) erl_eval.erl:670: :erl_eval.do_apply/6\n    (elixir) lib/code.ex:170: Code.eval_string/3\n\n"
   end
 
+  test "each code should compile and run in isolated environment" do
+    code1 = "defmodule Hello do; def hello() do; IO.puts(:hello); end; end; Hello.hello()"
+    assert CodeRunner.run(code1) == "hello\n"
+    code2 = "Hello.hello()"
+    assert CodeRunner.run(code2) == "** (UndefinedFunctionError) function Hello.hello/0 is undefined (module Hello is not available)\n    Hello.hello()\n    (stdlib) erl_eval.erl:670: :erl_eval.do_apply/6\n    (elixir) lib/code.ex:170: Code.eval_string/3\n\n"
+  end
+
   # Unit tests
   ~s{
     it compiles each time the code is sent. 
