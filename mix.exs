606b570fc77636ecae57576342c938662f96c9e8
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2017-08-15 20:26:25 +0900

    Update version to 0.1.1
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CodeRunner.Mixfile do
 
   def project do
     [app: :code_runner,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
